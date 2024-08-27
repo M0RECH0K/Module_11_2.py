b5caaceefa63940a18c22f732c98a514533984b3
Dmitry Kharlamov <168891004+M0RECH0K@users.noreply.github.com>
2024-08-27 15:29:53 +0300

    Update module_11_2.py

--- module_11_2.py
+++ module_11_2.py
@@ -1,12 +1,35 @@
+class IntrospectionInfo:
+    def __init__(self, obj):
+        self.obj = obj
+
+    def type_(self):
+        return type(self.obj)
+
+    def attributes_(self):
+        return dir(self.obj)
+
+    def methods_(self):
+        methods = []
+        for at in self.attributes_():
+            if at[0] != '_':
+                methods.append(at)
+        return methods
+
+    def module_(self):
+        module = self.obj, __class__.__module__
+        if module == 'builtins':
+            module = '__main__'
+        return module
+
+
 def introspection_info(obj):
-    obj_type = type(obj)
-    attributes = dir(obj)
-    methods = [method for method in dir(obj) if callable(getattr(obj, method))]
-    module = getattr(obj, '__module__', None)
-    return {'type': obj_type.__name__,
-            'attributes': attributes,
-            'methods': methods,
-            'module': module}
+    help_ = IntrospectionInfo(obj)
+    info = {
+        'type': help_.type_(),
+        'attributes': help_.attributes_(),
+        'methods': help_.methods_(),
+        'module': help_.module_()}
+    return info
 
 
 number_info = introspection_info(42)
