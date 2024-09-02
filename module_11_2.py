9dfb5522a97ccf83615e7f39670790847846ffea
Dmitry Kharlamov <168891004+M0RECH0K@users.noreply.github.com>
2024-09-02 09:12:45 +0300

    Update module_11_2.py

--- module_11_2.py
+++ module_11_2.py
@@ -1,27 +1,15 @@
 def introspection_info(obj):
-    type_obj = type(obj).__name__
-    attributes = dir(obj)
-    methods = [method for method in dir(obj) if callable(getattr(obj, method))]
-    module = getattr(obj, '__module__', None)
+    print(type(obj).__name__)
+    print(dir(obj))
+    print([method for method in dir(obj) if callable(getattr(obj, method))])
+    print(getattr(obj, '__module__', None))
     return {
-        'type': type_obj,
-        'attributes': attributes,
-        'methods': methods,
-        'module': module
+        'type': type(obj),
+        'attributes': dir(obj),
+        'methods': [method for method in dir(obj) if callable(getattr(obj, method))],
+        'module': getattr(obj, '__module__', None)
     }
 
 
 number_info = introspection_info(42)
 print(number_info)
-
-list_info = introspection_info([4, 1, 3])
-print(list_info)
-
-
-class IntrospectionInfo:
-    pass
-
-
-class_info = introspection_info(IntrospectionInfo)
-print(class_info)
-
